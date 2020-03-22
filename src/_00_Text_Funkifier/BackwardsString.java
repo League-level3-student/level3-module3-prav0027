b9f51f8a1d16d6a81db5095117958300882aa80d
prav0027 <39353735+prav0027@users.noreply.github.com>
2020-03-21 17:33:48 -0700

    BackwardsString(IP)

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,8 +8,31 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
+		String[] wordRay;
+		String output = "";
+		int wordcount=1;
+		String stringcopy = s;
+		int spaceindex = stringcopy.indexOf(" ");
+		while(spaceindex!=-1) {
+		stringcopy = stringcopy.substring(spaceindex+1);
+		spaceindex = stringcopy.indexOf(" ");
+		wordcount++;
+		}
+		stringcopy = s;
 		
-		return "";
+		wordRay = new String[wordcount];
+		spaceindex = stringcopy.indexOf(" ");
+		for(int i=0; i<wordcount-1; i++) {
+			wordRay[i] = stringcopy.substring(0, stringcopy.indexOf(" "));
+			stringcopy = stringcopy.substring(spaceindex+1);
+			spaceindex = stringcopy.indexOf(" ");
+		}
+		wordRay[wordcount-1] = stringcopy;
+		
+		for(int j=wordRay.length-1; j>=0; j--) {
+			//print each word backwards !!!
+		}
+		return output;
 	}
 
 }
