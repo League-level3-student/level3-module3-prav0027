ec78d4bfad86a427d75ce7798ffad431aa146119
prav0027 <39353735+prav0027@users.noreply.github.com>
2020-03-28 17:27:21 -0700

    MixedCaps, BackwardsString

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,34 +8,10 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
-		String[] wordRay;
 		String output = "";
-		int wordcount=1;
-		String stringcopy = s;
-		int spaceindex = stringcopy.indexOf(" ");
-		while(spaceindex!=-1) {
-		stringcopy = stringcopy.substring(spaceindex+1);
-		spaceindex = stringcopy.indexOf(" ");
-		wordcount++;
+		for(int i=s.length()-1; i>=0; i--) {
+			output = output+ s.charAt(i);
 		}
-		stringcopy = s;
-		
-		wordRay = new String[wordcount];
-		spaceindex = stringcopy.indexOf(" ");
-		for(int i=0; i<wordcount-1; i++) {
-			wordRay[i] = stringcopy.substring(0, stringcopy.indexOf(" "));
-			stringcopy = stringcopy.substring(spaceindex+1);
-			spaceindex = stringcopy.indexOf(" ");
-		}
-		wordRay[wordcount-1] = stringcopy;
-		
-		for(int j=wordRay.length-1; j>=0; j--) {
-			for(int k = wordRay[j].length()-1; k>=0; k--) {
-				output = output+wordRay[j].charAt(k);
-			}
-			output = output+" ";
-		}
-		System.out.println(output);
 		return output;
 	}
 
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -1,6 +1,6 @@
 package _00_Text_Funkifier;
 
-public class MixedCapsString extends SpecialString{
+public class MixedCapsString extends SpecialString {
 
 	public MixedCapsString(String s) {
 		super(s);
@@ -9,9 +9,27 @@ public class MixedCapsString extends SpecialString{
 	@Override
 	public String funkifyText(String s) {
 		String output = "";
-		
-		
-		
+		int addfirst = s.charAt(0)+32;
+		output = Character.toString((char) addfirst);
+		for (int i = 1; i < s.length(); i++) {
+			if (i % 2 == 1) {
+				if (s.charAt(i) == ' ') {
+					output = output + s.charAt(i);
+				} 
+				else if (s.charAt(i) < 97) {
+					int add = s.charAt(i) + 32;
+					output = output + (char) add;
+				} 
+				else {
+					int add = s.charAt(i) - 32;
+					output = output + (char) add;
+				}
+			} 
+			else {
+				output = output + s.charAt(i);
+			}
+		}
+
 		return output;
 	}
 
